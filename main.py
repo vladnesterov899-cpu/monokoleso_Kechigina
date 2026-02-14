02fe651f38d629760d4f6eeae3bb8aba9f3af5df
vladnesterov899-cpu <vladnesterov899@gmail.com>
2026-02-14 10:27:30 +0300

    BOtloh😁😁💕

--- main.py
+++ main.py
@@ -1,1 +1,41 @@
-print("poka")
+from aiogram import Bot, Dispatcher
+from aiogram.filters import Command
+from aiogram.types import Message
+
+# Вместо BOT TOKEN HERE нужно вставить токен вашего бота, полученный у @BotFather
+BOT_TOKEN = '7963812689:AAGH_wl5Kw9X3dtu1xdXRR7UxOJy9i-s6Ck'
+
+# Создаем объекты бота и диспетчера
+bot = Bot(token=BOT_TOKEN)
+dp = Dispatcher()
+
+
+# Этот хэндлер будет срабатывать на команду "/start"
+@dp.message(Command(commands="start"))
+async def process_start_command(message: Message):
+    await message.answer('Привет!\nМеня зовут Эхо-бот!\nНапиши мне что-нибудь')
+
+
+@dp.message(Command(commands="zachem"))
+async def process_zachem_command(message: Message):
+    await message.answer('Хз просто')
+
+
+# Этот хэндлер будет срабатывать на команду "/help"
+@dp.message(Command(commands="help"))
+async def process_help_command(message: Message):
+    await message.answer(
+        'Напиши мне что-нибудь и в ответ '
+        'я пришлю тебе твое сообщение'
+    )
+
+
+# Этот хэндлер будет срабатывать на любые ваши текстовые сообщения,
+# кроме команд "/start" и "/help"
+@dp.message()
+async def send_echo(message: Message):
+    await message.reply(text=message.text)
+
+
+if __name__ == '__main__':
+    dp.run_polling(bot)
